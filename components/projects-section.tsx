1681e8f38a2844d356e5a849cfb6d8f40b21caa9
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2025-10-29 13:09:34 -0300

    fix: fixing gap cards in project section

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -35,7 +35,7 @@ export async function ProjectsSection() {
   return (
     <section id="projects" className="container mx-auto z-10 py-12 px-4">
       <h2 className="text-3xl font-bold text-center mb-8">Meus Projetos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {repos
           .filter((repo) => repo.description)
           .map((repo) => (
